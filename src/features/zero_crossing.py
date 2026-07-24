1fe396da6e699a69e1cf2842f3bcc471e6e74a3d
AliHeidari2001b <143291007+AliHeidari2001@users.noreply.github.com>
2026-07-24 14:24:33 +0330

    completed day6 task

--- src/features/zero_crossing.py
+++ src/features/zero_crossing.py
@@ -7,10 +7,15 @@ audio, sr = librosa.load("data/voice.wav", sr=None)
 #محاسبه zero crossing rate
 zcr = librosa.feature.zero_crossing_rate(audio)
 
+#محاسبه میانگین
+zcr_mean = np.mean(zcr)
+
+#نمایش خروجی
+
 print(zcr.shape)
 
-print("zero crossing Rate:")
+print("zero crossing Rate:",zcr.shape)
 
-print("Average ZCR:")
+print("Average ZCR:",zcr_mean)
 
 print(np.mean(zcr))
