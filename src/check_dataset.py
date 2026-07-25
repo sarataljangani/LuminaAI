b5892886eb34b278ae0f1eccf5ed20ef96eb577f
sarataljangani <taljanganisara2@gmail.com>
2026-07-25 21:10:39 +0330

    day07

--- src/check_dataset.py
+++ src/check_dataset.py
@@ -2,16 +2,6 @@ import pandas as pd
 
 df = pd.read_csv("data/features.csv")
 
-<<<<<<< HEAD
-print(df.info())
-
-print("\n")
-
-print(df.describe())
-
-print("\n")
-
-=======
 print(df.head())
 
 print()
@@ -27,5 +17,4 @@ print(df.columns)
 print()
 
 print("Classes:")
->>>>>>> ec08a901f3cd2ab006c4ccf4a9df40ed1ab4dd79
 print(df["Person"].value_counts())
