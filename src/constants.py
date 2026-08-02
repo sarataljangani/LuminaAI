a83b0196b0c935bcbb962b62f638df8e1f99e3cd
sarataljangani <taljanganisara2@gmail.com>
2026-08-02 17:27:28 +0330

    Day12: Implement Feature Extraction Module

---
+++ src/constants.py
@@ -0,0 +1,3 @@
+from constants import N_MFCC
+DEFAULT_SAMPLE_RATE = None
+n_mfcc=N_MFCC
--- src/create_features_dataset.py
+++ src/create_features_dataset.py
@@ -1,8 +1,33 @@
+import os
 import pandas as pd
 
 from audio_loader import load_audio
-
 from feature_extractor import extract_features
-def create_dataset():
+
+
+def create_dataset(
+    labels_path,
+    audio_folder,
+    output_path
+):
+    """
+    Build feature dataset from audio files.
+    """
 
     pass
+labels = pd.read_csv(labels_path)
+
+print(labels.head())
+
+dataset = []
+{
+    "speaker_id": 12,
+
+    "mfcc_1": ...
+
+    "mfcc_2": ...
+
+    ...
+
+    "mfcc_13": ...
+}
